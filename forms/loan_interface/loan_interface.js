0b69d63a374d5209be8365974430457e47cad56b
Jrp85607 <54645651+Jrp85607@users.noreply.github.com>
2020-10-04 22:19:27 -0500

    Updates

--- forms/loan_interface/loan_interface.js
+++ forms/loan_interface/loan_interface.js
@@ -5,13 +5,12 @@ function carLoanPayment(principle, interest, n) {
   }
   
   
-  function homeLoanPayment(principle, i, n) {
+function homeLoanPayment(principle, interest, n) {
   return principle * interest / (1 - (Math.pow(1/(1 + interest), n)));
   }
   
-  let newLoan = "Yes"
+let newLoan = "Yes"
 let choice = " "
-
 
 let carAmount = " "
 let carInterest = " "
@@ -26,11 +25,11 @@ let homeFinalInterest = " "
 let homeFinalYear = " "
 let homeLoanAmount = " "
 let finalLoop = " "
-  
-  
+
+
 while (newLoan == "Yes") {
   
-    choice = prompt("Would you like to calculate a home loan or a car loan?")
+  choice = prompt("Would you like to calculate a home loan or a car loan?")
 
    if (choice =='Car' || choice == 'car') {
 
@@ -46,7 +45,7 @@ while (newLoan == "Yes") {
 
     } else if (choice =='Home' || choice == 'home') {
   
-          homeAmount = parseInt(prompt(" Enter the amount of the car loan, please no dollar signs or commas: "))
+          homeAmount = parseInt(prompt(" Enter the amount of the home loan, please no dollar signs or commas: "))
           homeInterest = parseFloat(prompt("Enter the interest rate in decimal formatting: "))
           homeYear = parseInt(prompt("Enter the amount of years to pay off the loan, either 15 or 30 years: "))
           homeFinalInterest = homeInterest / 1200
@@ -64,3 +63,42 @@ while (newLoan == "Yes") {
         
 }
 alert("Have a good day")
+
+
+/*
+inptPlay.onclick=function()
+{
+  if (inptPlay.value == "Play")  
+    {
+    userName= inptName.value
+    nounValue = nounArray[inptNoun.value] 
+    verbValue = verbArray[inptVerb.value] 
+    adjectiveValue = adjectiveArray[inptAdjective.value] 
+    adverbValue = adverbArray[inptAdverb.value] 
+    strDisplay0 = strMadLib.replace(/noun/g, nounValue)
+    strDisplay1 = strDisplay0.replace(/verb/g, verbValue)
+    strDisplay2 = strDisplay1.replace(/adjective/g, adjectiveValue)
+    strDisplay = strDisplay2.replace(/adverb/g, adverbValue)
+    lblName.textContent = `${userName}, here is your MadLib`
+    lblName.hidden = false
+    lblDisplay.textContent = strDisplay
+    lblDisplay.hidden = false
+    inptPlay.value = "Clear"
+    console.log(strDisplay)
+    }  else  
+    {
+    inptName.value = ""
+    inptNoun.value = ""
+    inptVerb.value = ""
+    inptAdjective.value = ""
+    inptAdverb.value = ""
+    lblName.textContent = ""
+    lblName.hidden = true
+    lblDisplay.textContent = ""
+    lblDisplay.hidden = true
+    inptPlay.value = "Play" */
+  /*  console.log(userName , nounValue, verbValue, adjectiveValue, adverbValue)*/
+ /*   }
+  
+}
+ */
